c1474c7dc2420967a1b873f6db191aa618b74bb8
armankabir <47779642+armankabir@users.noreply.github.com>
2020-09-05 22:26:06 -0400

    Initial Commit of App

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var billAmount:UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Changing keyboard to only have numbers
+        billAmountTextField.keyboardType = UIKeyboardType.decimalPad
     }
 
+    
+    @IBAction func onTap(_ sender: Any) {
+        
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //Obtaining input from user, from text field
+        let bill = Double(billAmountTextField.text!) ?? 0
+        //Percentages used to calculate tip
+        let tipPercentages = [0.15, 0.18, 0.20]
+        //tip amount, before adding to bill
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        //Tip percentage and bill become the total
+        let total = bill + tip
+        tipPercentageLabel.text = String( format: "$%.2f", tip)
+        totalLabel.text = String( format: "$%.2f", total)
+        
+    }
 
 }
 
