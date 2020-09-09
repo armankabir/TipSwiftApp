5d790fefa8992dc343bb0b1c77066544f7866b06
armankabir <47779642+armankabir@users.noreply.github.com>
2020-09-09 12:33:50 -0400

    Added Button Functionality. Replaced the text field with buttons to enter numbers, making the app more similar to a real calculator

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -12,13 +12,14 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipPercentageLabel: UILabel!
-    @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var billAmount:UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billLabel: UILabel!
+    var total = "";
     override func viewDidLoad() {
         super.viewDidLoad()
         //Changing keyboard to only have numbers
-        billAmountTextField.keyboardType = UIKeyboardType.decimalPad
+//        billAmountTextField.keyboardType = UIKeyboardType.decimalPad
     }
 
     
@@ -27,18 +28,96 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateTip(_ sender: Any) {
+        print("in calculate tip function")
         //Obtaining input from user, from text field
-        let bill = Double(billAmountTextField.text!) ?? 0
+        billLabel.text = String(total)
+        let bill = Double(total) ?? 0
         //Percentages used to calculate tip
         let tipPercentages = [0.15, 0.18, 0.20]
         //tip amount, before adding to bill
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         //Tip percentage and bill become the total
-        let total = bill + tip
+        let totalBill = bill + tip
         tipPercentageLabel.text = String( format: "$%.2f", tip)
-        totalLabel.text = String( format: "$%.2f", total)
-        
+        totalLabel.text = String( format: "$%.2f", totalBill)
     }
+//*************functions to handle button presses*************//
+    @IBAction func button1(_ sender: Any) {
+        print("1 pressed")
+        total += "1"
+        print("Total is now", total)
+        calculateTip(total)
+    }
+    @IBAction func button2(_ sender: Any) {
+        print("2 pressed")
+        total += "2"
+        print("Total is now", total)
+        calculateTip(total)
 
+    }
+    @IBAction func button3(_ sender: Any) {
+        print("3 pressed")
+        total += "3"
+        print("Total is now", total)
+        calculateTip(total)
+
+    }
+    @IBAction func button4(_ sender: Any) {
+        print("4 pressed")
+        total += "4"
+        print("Total is now", total)
+        calculateTip(total)
+
+    }
+    @IBAction func button5(_ sender: Any) {
+        print("5 pressed")
+        total += "5"
+        print("Total is now", total)
+        calculateTip(total)
+
+    }
+    @IBAction func button6(_ sender: Any) {
+        print("6 pressed")
+        total += "6"
+        print("Total is now", total)
+        calculateTip(total)
+
+    }
+    @IBAction func button7(_ sender: Any) {
+        print("7 pressed")
+        total += "7"
+        print("Total is now", total)
+        calculateTip(total)
+
+    }
+    @IBAction func button8(_ sender: Any) {
+        print("8 pressed")
+        total += "8"
+        print("Total is now", total)
+        calculateTip(total)
+    }
+    @IBAction func button9(_ sender: Any) {
+        print("9 pressed")
+        total += "9"
+        print("Total is now", total)
+        calculateTip(total)
+    }
+    @IBAction func button0(_ sender: Any) {
+        print("0 pressed")
+        total += "0"
+        print("Total is now", total)
+        calculateTip(total)
+    }
+    @IBAction func buttonDecimal(_ sender: Any) {
+            print(". pressed")
+            total += "."
+            print("Total is now", total)
+            calculateTip(total)
+    }
+    
+    @IBAction func clearButton(_ sender: Any) {
+        total = ""
+        calculateTip(total)
+    }
 }
 
