09378b0243aaac74337770036fdd8e319b638cf6
armankabir <47779642+armankabir@users.noreply.github.com>
2020-09-15 15:35:41 -0400

    Fixes bugs, adds UI and UX elements

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -16,32 +16,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billLabel: UILabel!
     var total = "";
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //changing background color of apps UI
         self.view.backgroundColor = UIColor.systemTeal
-    }
-
+    }//end viewDidLoad
     
-    @IBAction func onTap(_ sender: Any) {
-        
-    }
+    @IBAction func onTap(_ sender: Any) {}
     
+//--------------------calculateTip function-------------------------\\
     @IBAction func calculateTip(_ sender: Any) {
         print("in calculate tip function")
+        
         //Obtaining input from user
-        billLabel.text = String(total)
         let bill = Double(total) ?? 0
+        var intBill = 0
+        if(bill-floor(bill) >= 0){intBill = Int(bill)}
+        print("Bill is:", bill);
+        
+        //Sets Bill label to decimal only if the user inputs a decimal
+        billLabel.text = bill-floor(bill) == 0 ?
+            //if true             if False\\
+            "$"+String(intBill) : String(format: "$%.2f", bill)
+        
+        //if last char of total is a decimal, show the decimal on the label
+        if(total.suffix(1) == "."){billLabel.text = "$"+total}
+
         //Percentages used to calculate tip
         let tipPercentages = [0.15, 0.18, 0.20]
+        
         //tip amount, before adding to bill
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
         //Tip percentage and bill become the total
         let totalBill = bill + tip
         tipPercentageLabel.text = String( format: "$%.2f", tip)
         totalLabel.text = String( format: "$%.2f", totalBill)
-    }
-//*************Actions to handle button presses*************//
+    }//end function
+    
+//--------------------Actions to handle button presses-------------------\\
     @IBAction func button1(_ sender: Any) {
         print("1 pressed")
         total += "1"
@@ -109,10 +123,15 @@ class ViewController: UIViewController {
         calculateTip(total)
     }
     @IBAction func buttonDecimal(_ sender: Any) {
+        let doubleTotal = Double(total) ?? 0
+         //if statement checking if the number is already a decimal
+        //more than one decimal not added
+        if (doubleTotal - floor(doubleTotal) == 0){
             print(". pressed")
             total += "."
             print("Total is now", total)
             calculateTip(total)
+        }//end if
     }
     
     @IBAction func clearButton(_ sender: Any) {
